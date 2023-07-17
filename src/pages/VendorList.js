da5c606323b07238bd8b438450cef6113b26a554
Taraneh Biglari <taranehbiglari@gmail.com>
2023-07-17 18:34:53 +0330

    feat: create vendor card component

--- src/pages/VendorList.js
+++
@@ -1,6 +0,0 @@
-import { Link } from "react-router-dom";
-const VendorList = () => {
-  return <Link to={"/home"}>home</Link>;
-};
-
-export default VendorList;
---
+++ src/pages/VendorList/index.js
@@ -0,0 +1,8 @@
+import VendorCard from "../../components/VendorCard";
+import style from './style.module.scss';
+
+const VendorList = () => {
+  return <div className={style.list}><VendorCard /></div>
+};
+
+export default VendorList;
