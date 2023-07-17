e3549f67726f195fc1c7b1f35d77f76893f1e1ed
Taraneh Biglari <taranehbiglari@gmail.com>
2023-07-17 12:59:36 +0330

    feat: config project

--- src/App.js
+++ src/App.js
@@ -1,14 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import './assets/styles/main.scss';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+       <h1>ترانه</h1>
         <a
           className="App-link"
           href="https://reactjs.org"
